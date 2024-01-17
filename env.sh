e5e2785ab93219bd2f43102368ffaa471e19083d
Chew Boon Aik <bachew@gmail.com>
2024-01-17 14:04:28 +0800

    add py.typed marker file

--- env.sh
+++ env.sh
@@ -1,11 +1,11 @@
 # env.sh is not executed during bootstrapping, please create env.py instead, for example with:
 #
-# from os import environ as env
 # from pathlib import Path
+# import os
 #
 #
 # base_dir = Path(__file__).parent
-# env['DOCKER_CONFIG'] = str(base_dir / 'tmp/docker-config')
+# os.environ['DOCKER_CONFIG'] = str(base_dir / 'tmp/docker-config')
 #
 #
 # The variables set in env.py will be available in development, for example:
